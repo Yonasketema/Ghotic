e8dfa41d6b12840571e64cd67553f2f1f8ef0c1c
yonasketema <yonask.kitaw@gmail.com>
2022-11-23 22:03:00 +0300

    add art model

--- Backend/gallery/models.py
+++ Backend/gallery/models.py
@@ -7,3 +7,8 @@ class Artist(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     description = models.TextField()
+
+
+class Art(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    description = models.TextField()
