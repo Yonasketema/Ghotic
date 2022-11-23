10dc192485eff3f4ddbbdacacef6eaa13b4d839b
yonasketema <yonask.kitaw@gmail.com>
2022-11-23 21:54:07 +0300

    add artist model

--- Backend/gallery/models.py
+++ Backend/gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    description = models.TextField()
