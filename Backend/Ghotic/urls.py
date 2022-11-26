c4042ecf750abc4e8ef2a3dc77a61318234faa08
yonasketema <yonask.kitaw@gmail.com>
2022-11-26 21:55:19 +0300

    add new router

--- Backend/Ghotic/urls.py
+++ Backend/Ghotic/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gallery/', include('gallery.urls'))
+    path('', include('gallery.urls'))
 ]
--- Backend/gallery/urls.py
+++ Backend/gallery/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 from . import views
 
 routers = SimpleRouter()
 routers.register('products', views.ProductViewSet)
 
-urlpatterns = routers.urls
+
+# urlpatterns =
+
+urlpatterns = [
+    path('artist/<artist_name>/', views.artist)
+] + routers.urls
