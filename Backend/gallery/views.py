798c022862fdaac878e273c10dfb528384a10d86
yonasketema <yonask.kitaw@gmail.com>
2022-11-26 21:53:36 +0300

    feat:artist list api

--- Backend/gallery/views.py
+++ Backend/gallery/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from gallery.models import Product
+from gallery.models import Product, Artist
 from gallery.serializer import ProductSerializer
-
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
 
 # Create your views here.
 
@@ -14,3 +13,12 @@ from rest_framework.decorators import api_view
 class ProductViewSet(ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+@api_view()
+def artist(request, artist_name):
+    product = Product.objects.filter(artist__username=artist_name)
+    if not product:
+        return Response("no artist with is name", status.HTTP_404_NOT_FOUND)
+    serializer = ProductSerializer(product, many=True)
+    return Response(serializer.data)
