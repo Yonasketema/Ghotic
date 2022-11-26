89709e526840a9f8864c8aa7bac2fff4f9e8a88b
yonasketema <yonask.kitaw@gmail.com>
2022-11-26 21:55:46 +0300

    add username

--- Backend/gallery/models.py
+++ Backend/gallery/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Artist(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
     description = models.TextField()
 
 
