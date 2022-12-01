2aaa45f07303f47fbb805264f91bf081b6334983
yonasketema <yonask.kitaw@gmail.com>
2022-12-01 14:42:35 +0300

    add images serializer and images fields

--- Backend/gallery/serializer.py
+++ Backend/gallery/serializer.py
@@ -1,19 +1,35 @@
 from rest_framework import serializers
 
-from gallery.models import Artist, Product
+from gallery.models import Artist, ArtistImage, Product, ProductImage
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image']
+
+
+class ArtistImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ArtistImage
+        fields = ['id', 'image']
 
 
 class ArtistSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Artist
-        fields = ['id', 'first_name', 'last_name', 'description']
+        fields = ['id', 'first_name', 'last_name', 'description', 'images']
 
 
 class ProductSerializer(serializers.ModelSerializer):
     artist = ArtistSerializer()
     category = serializers.StringRelatedField()
+    images = ProductImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
-        fields = ['id', 'title', 'artist', 'category', 'description']
+        fields = ['id', 'title', 'artist', 'category', 'description', 'images']
