9d6e86e4cc26fcf3483580bafb14ec0151196c1a
yonasketema <yonask.kitaw@gmail.com>
2022-12-02 09:00:53 +0300

    add auth path

--- Backend/Ghotic/urls.py
+++ Backend/Ghotic/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gallery.urls'))
+    path('', include('gallery.urls')),
+    path('auth/', include('djoser.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
