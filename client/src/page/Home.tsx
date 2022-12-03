3d6782828a3adb7a18c8a199dfd0bd9a2c892f2e
yonasketema <yonask.kitaw@gmail.com>
2022-12-03 21:18:16 +0300

    global style

---
+++ client/src/page/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 1fr",
+        gridGap: "3rem",
+      }}
+    >
+      <h1>Yon</h1>
+    </div>
+  );
+};
+
+export default HomePage;
+
+//
