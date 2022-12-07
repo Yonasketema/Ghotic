723dc4efda9a05264f00e9d60a3d4c6fdb2b9387
yonasketema <yonask.kitaw@gmail.com>
2022-12-07 05:32:38 +0300

    fix:image

--- Backend/gallery/admin.py
+++ Backend/gallery/admin.py
@@ -3,13 +3,8 @@ from . import models
 # Register your models h@ere.
 
 
-class ArtistImageInline(admin.TabularInline):
-    model = models.ArtistImage
-
-
 @admin.register(models.Artist)
 class ArtistAdmin(admin.ModelAdmin):
-    inlines = [ArtistImageInline]
     list_display = ['first_name', 'last_name', 'username']
 
 
@@ -18,11 +13,7 @@ class ProductCategoryAdmin(admin.ModelAdmin):
     pass
 
 
-class ProductImageInline(admin.TabularInline):
-    model = models.ProductImage
-
-
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductImageInline]
+    # inlines = [ProductImageInline]
     list_display = ['title', 'artist']
--- Backend/gallery/models.py
+++ Backend/gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -6,17 +7,15 @@ from django.db import models
 class Artist(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
     description = models.TextField()
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(
+        upload_to='artist-profile/images')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-
-
-class ArtistImage(models.Model):
-    artist = models.ForeignKey(
-        Artist, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='artist/images')
 
 
 class ProductCategory(models.Model):
@@ -32,12 +31,7 @@ class Product(models.Model):
     category = models.ForeignKey(
         ProductCategory, on_delete=models.PROTECT)
     description = models.TextField()
+    images = models.ImageField(upload_to='product/images')
 
     def __str__(self) -> str:
         return self.title
-
-
-class ProductImage(models.Model):
-    artist = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='product/images')
