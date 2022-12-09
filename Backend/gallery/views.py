4c2519d19f9625448d5dbb9b57828399b62be04b
yonasketema <yonask.kitaw@gmail.com>
2022-12-09 14:03:21 +0300

    fix: permission | add likes

--- Backend/gallery/views.py
+++ Backend/gallery/views.py
@@ -1,6 +1,6 @@
 
 from pprint import pprint
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 
@@ -8,7 +8,7 @@ from gallery.models import Product, Artist
 from gallery.serializer import ArtistSerializer, ProductSerializer
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import api_view, action, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
@@ -34,11 +34,6 @@ class ArtistViewSet(CreateModelMixin, UpdateModelMixin, RetrieveModelMixin, Gene
     serializer_class = ArtistSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return [AllowAny()]
-        return [IsAuthenticated()]
-
     def get_serializer_context(self):
         return {'user_id': self.request.user.id, 'request': self.request}
 
@@ -46,11 +41,13 @@ class ArtistViewSet(CreateModelMixin, UpdateModelMixin, RetrieveModelMixin, Gene
     def me(self, request):
 
         (artist, created) = Artist.objects.get_or_create(user_id=request.user.id)
+
         if request.method == 'GET':
-            serializer = ArtistSerializer(artist)
+            serializer = ArtistSerializer(artist, context={'request': request})
             return Response(serializer.data)
         elif request.method == 'PUT':
-            serializer = ArtistSerializer(artist, data=request.data)
+            serializer = ArtistSerializer(
+                artist, data=request.data, context={'request': request})
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
@@ -63,3 +60,16 @@ def artist(request, artist_name):
         return Response("no artist with is name", status.HTTP_404_NOT_FOUND)
     serializer = ProductSerializer(product, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def product_like(request):
+    product = get_object_or_404(Product, id=request.data['product_id'])
+
+    if product.likes.filter(id=request.user.id).exists():
+        product.likes.remove(request.user)
+    else:
+        product.likes.add(request.user)
+
+    return Response(ProductSerializer(product, context={'request': request}).data,)
