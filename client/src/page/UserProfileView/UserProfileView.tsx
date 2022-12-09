2ecadce16943ce6ac115a95193893669ba159502
yonasketema <yonask.kitaw@gmail.com>
2022-12-09 14:11:43 +0300

    add userprofileview

---
+++ client/src/page/UserProfileView/UserProfileView.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Classes from "./userprofileview.module.css";
+
+function UserProfileView() {
+  return <div className={Classes.profileview_container}>UserProfileView</div>;
+}
+
+export default UserProfileView;
