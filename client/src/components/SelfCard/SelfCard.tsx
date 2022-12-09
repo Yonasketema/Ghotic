05be5c1b9191f205820ca70fcaf1b54eef5130db
yonasketema <yonask.kitaw@gmail.com>
2022-12-09 14:09:36 +0300

    add selfcard

---
+++ client/src/components/SelfCard/SelfCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Classes from "./selfcard.module.css";
+
+function SelfCard() {
+  return (
+    <div className={Classes.selfcard_container}>
+        <img src="https://picsum.photos/390/350" />
+        <div className={Classes.selfcard_status}>
+           <p>title of the year</p>
+            <button>
+              <i className="fa fa-heart" aria-hidden="true"></i> 
+            </button>
+        </div>
+       
+    </div>
+  );
+}
+
+export default SelfCard;
