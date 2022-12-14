932bbbcf46f323467a5000b95f04d768ae381906
yonasketema <yonask.kitaw@gmail.com>
2022-12-14 21:14:32 +0300

    add components lib

---
+++ client/src/components/lib.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+   
+  background-color: ${props => props.primary ?  "#BBD915" : "white"};
+  color: ${props => props.primary ? "#FFF" : "black"};
+  border-radius: ${props => props.rounded ? "5px" : "0"};
+
+
+  
+  font-size:1rem;
+  font-size: 1em;
+  padding: .5rem 1.7rem;
+  border: none;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px -50px 36px -28px inset;
+`;
+
+
+export {
+	Button
+ }
