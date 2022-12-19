d8c5589f7a12db018bce5ac50ff294c94d26bd6d
yonasketema <yonask.kitaw@gmail.com>
2022-12-19 19:51:04 +0300

    fix: cors

--- Backend/Ghotic/settings.py
+++ Backend/Ghotic/settings.py
@@ -42,17 +42,25 @@ INSTALLED_APPS = [
     'core',
     'gallery',
     'rest_framework',
-    'djoser'
+    'djoser',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'Ghotic.urls'
