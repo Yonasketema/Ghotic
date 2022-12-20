65f8169e2753dac8938ea020246b57f8941f3cc0
yonasketema <yonask.kitaw@gmail.com>
2022-12-20 22:38:42 +0300

    add user

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,53 +1,74 @@
 import React from "react";
 import Classes from "./header.module.css";
-import {Button ,FlexBox} from '../lib'
+import { Button, FlexBox } from "../lib";
 import styled from "styled-components";
-
 
 import { Link } from "react-router-dom";
 
+type User = {
+  id: number;
+  first_name?: string;
+  last_name?: string;
+  username?: string;
+  description?: string;
+  profile_pic?: string | null;
+};
 
-function Header() {
+type HeaderProp = {
+  User: User;
+};
+
+function Header({ User }: HeaderProp) {
+  function logout() {
+    window.localStorage.removeItem("Ghotic_token");
+    window.location.assign("/");
+  }
+
   return (
     <header className={Classes.header_container}>
-     <nav className={Classes.header_nav}>
-        <div>Gothic</div>
+      <nav className={Classes.header_nav}>
+        <Link to="/">Gothic</Link>
 
         <div>Home</div>
         <div>About</div>
       </nav>
 
       <div>
-        {false ? (
+        {User ? (
           <div className={Classes.profile_dropdown}>
             <div className={Classes.profile_img}>
-              <img
-                src="https://picsum.photos/323/223"
-              />
-               <div className={Classes.profile_subnav}>
-                <Link to='/artistprofile'><p>Profile</p> </Link>
-                <p>Edit Profile</p>
-                <p><i className="fa fa-heart" aria-hidden="true"></i>  My Like</p>
-                <p>Sign out</p>
+              <img src="https://picsum.photos/323/223" />
+              <small>{User?.username}</small>
+              <div className={Classes.profile_subnav}>
+                <Link to="/artistprofile">
+                  <p>Profile</p>{" "}
+                </Link>
+                <Link to="/editprofile">
+                  {" "}
+                  <p>Edit Profile</p>
+                </Link>
+                <p>
+                  <i className="fa fa-heart" aria-hidden="true"></i> My Like
+                </p>
+                <p onClick={logout}>Sign out</p>
               </div>
-
             </div>
 
             <Link to="/upload">
-               <Button primary rounded >Upload</Button>
+              <Button primary rounded>
+                Upload
+              </Button>
             </Link>
-            
-
           </div>
         ) : (
-
           <FlexBox>
-            <Button primary  >Sign up</Button>
-           <Button primary  >Login</Button>
-          </FlexBox> 
-
-           
-          
+            <Link to="/register">
+              <Button primary>Register</Button>
+            </Link>
+            <Link to="/login">
+              <Button primary>Login</Button>
+            </Link>
+          </FlexBox>
         )}
       </div>
     </header>
