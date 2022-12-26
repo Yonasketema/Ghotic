e940b1dbb15e15e57a418e19d73ac67184ac43ad
yonasketema <yonask.kitaw@gmail.com>
2022-12-26 21:19:33 +0300

    add artist profile api endpoint | proptype

--- client/src/page/ArtistProfile/ArtistProfile.tsx
+++ client/src/page/ArtistProfile/ArtistProfile.tsx
@@ -1,10 +1,43 @@
 import React from "react";
 import Classes from "./artistProfile.module.css";
 
+import { useQuery ,useMutation,useQueryClient} from "@tanstack/react-query";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 import SelfCard from "./../../components/SelfCard/SelfCard";
 import Profile from "./../../components/profile/Profile";
 
+
+type Product = {
+  id: number;
+  title: string;
+  artist: {
+    id: number;
+    first_name: string;
+    last_name: number;
+    username: string;
+    description: string;
+    profile_pic: string | null;
+  };
+  category: number;
+  description: string;
+  images: string;
+  likes: number[];
+  likes_number:number;
+};
+
+
+
 function ArtistProfile() {
+
+  const { artist_username } = useParams();
+
+
+    const { isLoading, error, data } = useQuery<Product[]>(["artist",artist_username], () =>
+    axios.get(`http://127.0.0.1:8001/gallery/artist/${artist_username}`).then((res) => res.data)
+  );
+
   return (
     <div className={Classes.artistprofile_container}>
       <Profile />
@@ -21,10 +54,27 @@ function ArtistProfile() {
           </ul>
         </div>
 
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <SelfCard />
-          <SelfCard />
-          <SelfCard />
+        <div 
+        style={{
+               
+              display: "Grid",
+               gridTemplateColumns: "repeat(3, 1fr)",
+               gridAutoFlow:"dense",
+              justifyContent: "space-between",
+              width: "100%",
+              gap: "1rem",
+            }}>
+          {data?.map((product , i) => (
+              <SelfCard 
+              key={product.id}  
+              // onPressLove={handleLike} 
+              id={product.id}
+              title={product.title}
+              images={product.images}
+              
+              />
+            ))}
+        
         </div>
       </main>
     </div>
