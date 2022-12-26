e0a65780159427650229fafd3b4422741ee41fda
yonasketema <yonask.kitaw@gmail.com>
2022-12-26 21:12:56 +0300

    add Like mutation

--- client/src/page/Home.tsx
+++ client/src/page/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./../components/card/Card";
 import SideBar from "./../components/sidebar/SideBar";
 import Screen from "./../components/Screen";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery ,useMutation,useQueryClient} from "@tanstack/react-query";
 import axios from "axios";
 
 type Product = {
@@ -19,13 +19,31 @@ type Product = {
   category: number;
   description: string;
   images: string;
-  likes: number;
+  likes: number[];
+  likes_number:number;
 };
 
-const HomePage = () => {
+const HomePage = (props: { token?: string }) => {
+  const queryClient = useQueryClient()
+
   const { isLoading, error, data } = useQuery<Product[]>(["product"], () =>
     axios.get(`${process.env.REACT_APP_PRODUCTS}`).then((res) => res.data)
   );
+
+  const handleLike = useMutation((product_id) =>
+    axios.post(`http://127.0.0.1:8001/gallery/like`, product_id,
+    {
+          headers: {
+            Authorization: `JWT ${props.token}`,
+          },
+    }),{
+      onSuccess: () => {
+        
+        queryClient.invalidateQueries(['product'])
+      },
+    }
+  )
+
 
   return (
     <>
@@ -45,14 +63,17 @@ const HomePage = () => {
           <div
             className="App"
             style={{
-              display: "flex",
+               
+              display: "Grid",
+               gridTemplateColumns: "repeat(3, 1fr)",
+               gridAutoFlow:"dense",
               justifyContent: "space-between",
               width: "100%",
               gap: "1rem",
             }}
           >
-            {data?.map((product) => (
-              <Card key={product.id} Product={product} />
+            {data?.map((product , i) => (
+              <Card key={product.id} Product={product} onPressLove={handleLike} />
             ))}
           </div>
           <div style={{ height: "1rem" }}></div>
