403efc129653cd6c6f468472cf6789c337764dea
yonasketema <yonask.kitaw@gmail.com>
2022-12-30 19:28:32 +0300

    add user email and artist works

--- Backend/gallery/serializer.py
+++ Backend/gallery/serializer.py
@@ -1,19 +1,29 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+from rest_framework.response import Response
 
 from gallery.models import Artist, Product, ProductCategory
-
+from pprint import pprint
 
 class ArtistSerializer(serializers.ModelSerializer):
+    artist_works = serializers.SerializerMethodField(method_name='works')
+    user_email =serializers.SerializerMethodField(method_name='email')
+  
+    def works(self, artist: Artist):
+        return artist.productbyartist.count()
+    def email(self,artist:Artist):
+        return artist.user.email    
+
 
     def create(self, validated_data):
         user_id = self.context['user_id']
         return Artist.objects.create(user_id=user_id, **validated_data)
 
+
     class Meta:
         model = Artist
         fields = ['id', 'first_name', 'last_name', 'username',
-                  'description', 'profile_pic']
+                  'description', 'profile_pic','artist_works','user_email']
 
 
 class ProductCategorySerializer(serializers.ModelSerializer):
