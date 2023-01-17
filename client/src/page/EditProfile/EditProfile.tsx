0b6dba8e7d5fa837a978d65e7f5dfdd04e8b8597
yonasketema <yonask.kitaw@gmail.com>
2023-01-17 11:54:58 +0300

    add profile pic service

--- client/src/page/EditProfile/EditProfile.tsx
+++ client/src/page/EditProfile/EditProfile.tsx
@@ -4,47 +4,79 @@ import { Button, Input } from "../../components/lib";
 import axios, { AxiosResponse } from "axios";
 
 function EditProfile(props: { token?: string }) {
-
-  
-    function handleProfileSubmit(event: any) {
+  function handleProfile(event: any) {
     event.preventDefault();
-    const { first_name, last_name ,username,description} = event.target.elements;
+    const { first_name, last_name, username, description } =
+      event.target.elements;
 
-    console.log(first_name.value,username.value)
+    console.log(first_name.value, username.value);
 
     axios
-      .put(`${process.env.REACT_APP_ME}`, {
-        
-  
-        first_name: first_name.value,
-        last_name: last_name.value ,
-        username: username.value,
-        description: description.value,
-     
-
-      },{
+      .put(
+        `${process.env.REACT_APP_ME}`,
+        {
+          first_name: first_name.value,
+          last_name: last_name.value,
+          username: username.value,
+          description: description.value,
+        },
+        {
           headers: {
             Authorization: `JWT ${props.token}`,
           },
-        })
+        }
+      )
       .then((res: AxiosResponse) => {
         console.log("**", res.data);
-       
       })
       .catch((error) => {
         console.error("XX", error.response.data);
       });
   }
+
+  function handleProfile_pic(event: any) {
+    event.preventDefault();
+
+    const { images } = event.target.elements;
+
+    const formData = new FormData();
+
+    formData.append("profile_pic", images.files[0]);
+
+    axios
+      .patch(
+        `http://127.0.0.1:8001/gallery/profile_pic`,
+
+        formData,
+        {
+          headers: {
+            Authorization: `JWT ${props.token}`,
+          },
+        }
+      )
+      .then((res) => res.data)
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div className={Classes.editprofile_container}>
-      <div className={Classes.profile_container}>
+      <form className={Classes.profile_container} onSubmit={handleProfile_pic}>
         <img src="https://picsum.photos/323/223" alt="profile" />
-        <Button rounded primary>
+        <Button type="submit" rounded primary>
           Upload new picture
         </Button>
-
-        <Button rounded>Delete</Button>
-      </div>
+        <input
+          // style={{ display: "none" }}
+          type="file"
+          accept="image/*"
+          name="image-upload"
+          id="images"
+          // onChange={imageHandler}
+        />
+        <Button type="button" rounded>
+          Delete
+        </Button>
+      </form>
 
       <div>
         <h4>General</h4>
@@ -70,47 +102,46 @@ function EditProfile(props: { token?: string }) {
 
       <h4>Profiles</h4>
 
- <form onSubmit={handleProfileSubmit}>
-         <label>Username</label>
-            <Input id="username" />
-            {/* <p>Your Ghotic URL: https://Ghotic.com/yonask</p> */}
+      <form onSubmit={handleProfile}>
+        <label>Username</label>
+        <Input id="username" />
+        {/* <p>Your Ghotic URL: https://Ghotic.com/yonask</p> */}
 
-      <div className={Classes.box_container}>
-            
-            <label>
-              First Name
-              <Input id="first_name"/>
-            </label>
-
-            <label>
-              Last Name
-              <Input id="last_name"/>
-            </label>
-          </div>
+        <div className={Classes.box_container}>
           <label>
-            Bio
-            <Input id="description" />
-          </label>
-          <h4>Social Profiles</h4>
-          <label>
-            Twitter
-            <Input />
+            First Name
+            <Input id="first_name" />
           </label>
 
           <label>
-            Facebook
-            <Input />
+            Last Name
+            <Input id="last_name" />
           </label>
+        </div>
+        <label>
+          Bio
+          <Input id="description" />
+        </label>
+        <h4>Social Profiles</h4>
+        <label>
+          Twitter
+          <Input />
+        </label>
 
-          <label>
-            Instagram
-            <Input />
-          </label>
+        <label>
+          Facebook
+          <Input />
+        </label>
 
-          <Button rounded primary>
-            Update Profile
-          </Button>
-     </form> 
+        <label>
+          Instagram
+          <Input />
+        </label>
+
+        <Button rounded primary>
+          Update Profile
+        </Button>
+      </form>
     </div>
   );
 }
