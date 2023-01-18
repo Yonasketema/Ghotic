970c990cb5928ed49023106ed2aaf38bc149d6a1
yonasketema <yonask.kitaw@gmail.com>
2023-01-18 16:44:22 +0300

    add product api layer

---
+++ client/src/apis/productApi.ts
@@ -0,0 +1,25 @@
+import { apiClient } from "./config/axiosConfig";
+
+const productUrl = "/gallery/products/";
+
+const getAllProduct = () => {
+  return apiClient.get(productUrl);
+};
+
+const getProduct = (id: string | undefined) => {
+  return apiClient.get(`${productUrl}${id}`);
+};
+
+const createProduct = (formData: any, token: string | undefined) => {
+  return apiClient.post(productUrl, formData, {
+    headers: {
+      Authorization: `JWT ${token}`,
+    },
+  });
+};
+
+export default {
+  getAllProduct,
+  getProduct,
+  createProduct,
+};
