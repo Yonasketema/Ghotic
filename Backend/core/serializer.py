e6fe55fe8ae3a271466f923ff8a56aab2fb6897e
yonasketema <yonask.kitaw@gmail.com>
2023-01-30 13:35:12 +0300

    delete unused comment  based on #1

--- Backend/core/serializer.py
+++ Backend/core/serializer.py
@@ -5,15 +5,5 @@ from rest_framework import exceptions, serializers
 
 class UserCreateSerializer(BaseUserCreateSerializer):
 
-    # def validate(self, attrs):
-    #     password = attrs.get("password")
-    #     password_confirm = attrs.get("password_confirm")
-
-    #     if password != password_confirm:
-    #         raise exceptions.ValidationError('password are not same')
-    #     else:
-
-    #         return super().validate(attrs)
-
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'email', 'password']
