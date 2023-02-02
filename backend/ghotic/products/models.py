3bc4417ca0af8a518d6d20b46cc70d4123243730
yonasketema <yonask.kitaw@gmail.com>
2023-02-02 21:34:12 +0300

    refactor: add products+productcategory `models`

--- backend/ghotic/products/models.py
+++ backend/ghotic/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from artists.models import Artist
+# from users.models import User
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=255)
 
-# Create your models here.
+    def __str__(self) -> str:
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE,related_name="productbyartist")
+    category = models.ForeignKey(
+        ProductCategory, on_delete=models.PROTECT)
+    description = models.TextField()
+    images = models.ImageField(upload_to='product/images')
+    likes = models.ManyToManyField(
+        Artist, related_name='product_like', default=None, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
