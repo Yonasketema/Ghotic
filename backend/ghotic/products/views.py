f922700e3d119de06b249cda696c262573391229
yonasketema <yonask.kitaw@gmail.com>
2023-02-02 21:38:13 +0300

    refactor: add product views

--- backend/ghotic/products/views.py
+++ backend/ghotic/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet, ModelViewSet
 
-# Create your views here.
+
+from .models import Product
+from .serializer import  ProductSerializer
+
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id, 'request': self.request}
