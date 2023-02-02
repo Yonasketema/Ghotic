85b310147943072c908eb08512bea42952b91c0a
yonasketema <yonask.kitaw@gmail.com>
2023-02-02 21:40:45 +0300

    refactor: added product admin

--- backend/ghotic/products/admin.py
+++ backend/ghotic/products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    # inlines = [ProductImageInline]
+    list_display = ['title', 'artist']
