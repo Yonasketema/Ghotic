c419535103f449e16d89946ca763c326412f01a6
yonasketema <yonask.kitaw@gmail.com>
2023-02-02 18:05:20 +0300

    fix: Direct assignment + product upload

--- backend/ghotic/gallery/serializer.py
+++ backend/ghotic/gallery/serializer.py
@@ -43,10 +43,15 @@ class ProductSerializer(serializers.ModelSerializer):
         user_id = self.context['user_id']
         artist = get_object_or_404(Artist,
                                    user_id=user_id)  # req.user.id
+        
+        data = validated_data
+        del data['likes']
+        product = Product.objects.create(artist_id=artist.id, **data)
+         
 
-        return Product.objects.create(artist_id=artist.id, **validated_data)
+        return product
 
     class Meta:
         model = Product
         fields = ['id', 'title', 'artist', 'category',
-                  'description', 'images', 'likes_number', 'likes']
+                  'description', 'images', 'likes_number','likes']
