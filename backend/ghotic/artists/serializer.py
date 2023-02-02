84496072839b4196f5f0bdd5274e5504be84ec4a
yonasketema <yonask.kitaw@gmail.com>
2023-02-02 21:46:41 +0300

    refactor: add artist `serializer.py`

---
+++ backend/ghotic/artists/serializer.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from rest_framework.response import Response
+
+from .models import Artist
+
+class ArtistSerializer(serializers.ModelSerializer):
+    artist_works = serializers.SerializerMethodField(method_name='works')
+    user_email = serializers.SerializerMethodField(method_name='email')
+
+    def works(self, artist: Artist):
+        return artist.productbyartist.count()
+
+    def email(self, artist: Artist):
+        return artist.user.email
+
+    def create(self, validated_data):
+        user_id = self.context['user_id']
+        return Artist.objects.create(user_id=user_id, **validated_data)
+
+    class Meta:
+        model = Artist
+        fields = ['id', 'first_name', 'last_name', 'username',
+                  'description', 'profile_pic', 'artist_works', 'user_email']
+
