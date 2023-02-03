866e23aaf8f4969457071ef633846b546b17457d
yonasketema <yonask.kitaw@gmail.com>
2023-02-03 14:13:21 +0300

    refactor: register product

--- backend/ghotic/products/admin.py
+++ backend/ghotic/products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 
+
 @admin.register(models.ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
     pass
@@ -8,5 +9,4 @@ class ProductCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    # inlines = [ProductImageInline]
     list_display = ['title', 'artist']
