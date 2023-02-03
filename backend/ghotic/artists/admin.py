e4c69dc71b81d6567415800d916c37d0924307a3
yonasketema <yonask.kitaw@gmail.com>
2023-02-03 14:11:47 +0300

    refactor: register artistadmin

--- backend/ghotic/artists/admin.py
+++ backend/ghotic/artists/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'username']
