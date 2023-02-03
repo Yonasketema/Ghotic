b4784ceb6394988db76ef8ce57637ac2cf23022f
yonasketema <yonask.kitaw@gmail.com>
2023-02-03 14:11:04 +0300

    refactor: add artist

--- backend/ghotic/artists/models.py
+++ backend/ghotic/artists/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True, null=True)
+    description = models.TextField()
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(
+        upload_to='artist-profile/images', default=None, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
