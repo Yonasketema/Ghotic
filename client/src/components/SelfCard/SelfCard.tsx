6412117462e73e0657447536a9d76e84c5519290
yonasketema <yonask.kitaw@gmail.com>
2023-02-08 22:03:55 +0300

    fix: update color

--- client/src/components/SelfCard/SelfCard.tsx
+++ client/src/components/SelfCard/SelfCard.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import Classes from "./selfcard.module.css";
-
+import { useQuery } from "@tanstack/react-query";
+import { User } from "../../page/shared/types/User";
 
 type selfProduct = {
   id: number;
   title: string;
   images: string;
-  // onPressLove:any
+  onPressLove: any;
+  likes: number[];
 };
 
+function SelfCard({ id, title, images, onPressLove, likes }: selfProduct) {
+  const { data } = useQuery<User>(["user"]);
 
-function SelfCard({id,title,images}:selfProduct) {
   return (
     <div className={Classes.selfcard_container}>
-      <img src={images}/>
+      <img src={images} />
       <div className={Classes.selfcard_status}>
         <p>{title}</p>
         <button>
-          <i className="fa fa-heart" aria-hidden="true"></i>
+          <i
+            className="fa fa-heart"
+            aria-hidden="true"
+            onClick={() => onPressLove.mutate({ product_id: id })}
+            style={{
+              color: likes?.includes(data ? data.id : 0) ? "#fb3958" : "#9999",
+            }}
+          ></i>
         </button>
       </div>
     </div>
