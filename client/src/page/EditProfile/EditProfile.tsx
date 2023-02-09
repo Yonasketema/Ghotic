628f0e4d151a040316737119f02657e59cd2ec80
yonasketema <yonask.kitaw@gmail.com>
2023-02-09 23:02:20 +0300

    refactor: edit forms

--- client/src/page/EditProfile/EditProfile.tsx
+++ client/src/page/EditProfile/EditProfile.tsx
@@ -6,140 +6,127 @@ import { Button, Input } from "../../components/lib";
 import { useQuery } from "@tanstack/react-query";
 import userAPI from "../../apis/userAPI";
 
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
+
 function EditProfile(props: { token?: string }) {
+  const { data } = useQuery(["user"]);
+
+  const [username, setUserName] = React.useState(data.username);
+
   function handleProfile(event: any) {
     event.preventDefault();
+
     const { first_name, last_name, username, description } =
       event.target.elements;
-
-    userAPI
-      .editArtistProfile(
-        {
-          first_name: first_name.value,
-          last_name: last_name.value,
-          username: username.value,
-          description: description.value,
-        },
-        props.token
-      )
-      .then((res: AxiosResponse) => {
-        console.log("**", res.data);
-      })
-      .catch((error) => {
-        console.error("XX", error.response.data);
-      });
-  }
-
-  function handleProfile_pic(event: any) {
-    event.preventDefault();
-
-    const { images } = event.target.elements;
-
-    const formData = new FormData();
-
-    formData.append("profile_pic", images.files[0]);
-
-    axios
-      .patch(
-        `http://127.0.0.1:8001/gallery/profile_pic`,
-
-        formData,
-        {
-          headers: {
-            Authorization: `JWT ${props.token}`,
-          },
-        }
-      )
-      .then((res) => res.data)
-      .catch((error) => console.log(error));
   }
 
   return (
-    <div className={Classes.editprofile_container}>
-      <form className={Classes.profile_container} onSubmit={handleProfile_pic}>
-        <img src="https://picsum.photos/323/223" alt="profile" />
-        <Button type="submit" rounded primary>
-          Upload new picture
-        </Button>
-        <input
-          // style={{ display: "none" }}
-          type="file"
-          accept="image/*"
-          name="image-upload"
-          id="images"
-          // onChange={imageHandler}
-        />
-        <Button type="button" rounded>
-          Delete
-        </Button>
-      </form>
+    <Formik
+      initialValues={{
+        first_name: data.first_name,
+        last_name: data.last_name,
+        username: data.username,
+        description: data.description,
+      }}
+      // validationSchema={Yup.object({})}
+      onSubmit={(values, { setSubmitting }) => {
+        setSubmitting(false);
+        userAPI
+          .editArtistProfile(
+            {
+              first_name: values.first_name,
+              last_name: values.last_name,
+              username: values.username,
+              description: values.description,
+            },
+            props.token
+          )
+          .then((res: AxiosResponse) => {
+            toast.success("You have successfully updated your profile.");
+          })
+          .catch((error) => {
+            console.error("XX", error.response.data);
+            toast.error("post deleted ");
+          });
+      }}
+    >
+      <div className={Classes.editprofile_container}>
+        <ToastContainer />
+        <h4>Profiles</h4>
 
-      <div>
-        <h4>General</h4>
-        <label>Email</label>
+        <Form className={Classes.editprofile_form}>
+          <label>Username</label>
 
-        <Input />
-      </div>
+          <Field name="username" id="username" type="text" />
 
-      <div className={Classes.box_container}>
-        <label>
-          Old password
-          <Input type="password" />
-        </label>
+          <p className={Classes.profile_url}>
+            Your Ghotic URL: https://Ghotic.com/{username}
+          </p>
 
-        <label>
-          Password
-          <Input type="password" />
-        </label>
-      </div>
-      <Button rounded primary>
-        Change
-      </Button>
+          <div className={Classes.box_container}>
+            <label>
+              First Name
+              <Field name="first_name" type="text" id="first_name" />
+            </label>
 
-      <h4>Profiles</h4>
+            <label>
+              Last Name
+              <Field name="last_name" type="text" id="last_name" />
+            </label>
+          </div>
 
-      <form onSubmit={handleProfile}>
-        <label>Username</label>
-        <Input id="username" placeholder="username" />
-        {/* <p>Your Ghotic URL: https://Ghotic.com/yonask</p> */}
+          <label>Bio </label>
+          <Field name="description" type="text" id="description" />
 
-        <div className={Classes.box_container}>
+          <h4>Social Profiles</h4>
           <label>
-            First Name
-            <Input id="first_name" />
+            Twitter
+            <Input />
           </label>
 
           <label>
-            Last Name
-            <Input id="last_name" />
+            Facebook
+            <Input />
           </label>
-        </div>
-        <label>
-          Bio
-          <Input id="description" />
-        </label>
-        <h4>Social Profiles</h4>
-        <label>
-          Twitter
-          <Input />
-        </label>
 
-        <label>
-          Facebook
-          <Input />
-        </label>
+          <label>
+            Instagram
+            <Input />
+          </label>
 
-        <label>
-          Instagram
-          <Input />
-        </label>
-
-        <Button rounded primary>
-          Update Profile
-        </Button>
-      </form>
-    </div>
+          <Button rounded primary type="submit">
+            Update Profile
+          </Button>
+        </Form>
+      </div>
+    </Formik>
   );
 }
 
 export default EditProfile;
+
+{
+  /* <div>
+<h4>General</h4>
+<label>Email</label>
+
+<Input />
+</div>
+
+<div className={Classes.box_container}>
+<label>
+  Old password
+  <Field name="password" type="password" />
+</label>
+
+<label>
+  Password
+  <Input type="password" />
+</label>
+</div>
+<Button rounded primary>
+Change
+</Button> */
+}
